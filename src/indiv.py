fcf81fc53f0b77f2efea32c0b291674d9475625d
lie544 <145787178+rcbr230@users.noreply.github.com>
2024-11-06 23:36:49 -0500

    Update indiv.py
    added in more sensory grabbing and tanh

--- src/indiv.py
+++ src/indiv.py
@@ -143,6 +143,115 @@ class indiv:
         return connections,weights
     
 
+    def genSensoryData(self,grid,age):
+        # Age = age
+        if len(self.sensoryConnections[0]) != 0:
+            self.sensoryValues[0] = age
+
+        # Rnd = random input
+        if len(self.sensoryConnections[1]) != 0:
+            self.sensoryValues[1] = random.randint(0,1000)
+
+        # Blr = blockage left-right
+        if len(self.sensoryConnections[2]) != 0:    
+            if self.lastMoveDir == 0 or self.lastMoveDir == 2:
+                left = self.loc[0]-1
+                right = self.loc[0]+1
+                if left > 0:
+                    self.sensoryValues[2] = grid.gridInfo[left][self.loc[1]]
+                if right < grid.sizeX:
+                    self.sensoryValues[2] = grid.gridInfo[right][self.loc[1]]
+            else:
+                left = self.loc[1]-1
+                right = self.loc[1]+1
+                if left > 0:
+                    self.sensoryValues[2] = grid.gridInfo[self.loc[0]][left]
+                if right < grid.sizeY:
+                    self.sensoryValues[2] = grid.gridInfo[self.loc[1]][right]     
+        if self.sensoryValues[2] > 0:
+            self.sensoryValues[2] = 1
+
+        # Osc = oscillator
+        if len(self.sensoryConnections[3]) != 0:
+            self.curOsc = not self.curOsc
+            self.sensoryValues[3] = self.curOsc
+
+        # Bfd = blockage forward
+        if len(self.sensoryConnections[4]) != 0:
+            if self.lastMoveDir == 0:
+                front = self.loc[1]+1
+                if front > 0:
+                    self.sensoryValues[4] = grid.gridInfo[front][self.loc[1]]
+            elif self.lastMoveDir == 1:
+                front = self.loc[0]+1
+                if front > 0:
+                    self.sensoryValues[4] = grid.gridInfo[self.loc[0]][front]
+            elif self.lastMoveDir == 2:
+                front = self.loc[1]-1
+                if front > 0:
+                    self.sensoryValues[4] = grid.gridInfo[front][self.loc[1]]
+            elif self.lastMoveDir == 3:
+                front = self.loc[1]-1
+                if front > 0:
+                    self.sensoryValues[4] = grid.gridInfo[self.loc[0]][front]
+            if self.sensoryValues[4] > 0:
+                self.sensoryValues[4] = 1
+
+                                                                # Plr = population gradient left-right
+        if len(self.sensoryConnections[5]) != 0:
+            pass
+                                                                # Pop = population density
+        if len(self.sensoryConnections[6]) != 0:
+            pass
+                                                                # Pfd = population gradient forward
+        if len(self.sensoryConnections[7]) != 0:
+            pass
+                                                                # LPf = population long-range forward
+        if len(self.sensoryConnections[8]) != 0:
+            pass
+        # LMy = last movement Y
+        if len(self.sensoryConnections[9]) != 0:
+            if self.lastMoveDir == 0 or self.lastMoveDir == 2:
+                self.sensoryValues[9] = 0
+            else:
+                self.sensoryValues[9] += 1
+
+                                                                # LBf = blockage long-range forward
+        if len(self.sensoryConnections[10]) != 0:
+            pass
+        # LMx = last movement X
+        if len(self.sensoryConnections[11]) != 0:
+            if self.lastMoveDir == 1 or self.lastMoveDir == 3:
+                self.sensoryValues[11] = 0
+            else:
+                self.sensoryValues[11] += 1
+
+        # BDy = north/south border distance
+        if len(self.sensoryConnections[12]) != 0:
+            self.sensoryValues[12] = min(self.loc[1], grid.sizeY-self.loc[1])
+
+                                                                # Gen = genetic similarity of forward neighbor 
+        if len(self.sensoryConnections[13]) != 0:
+            pass
+        
+        # BDx = east/west border distance
+        if len(self.sensoryConnections[14]) != 0:
+            self.sensoryValues[14] = min(self.loc[0], grid.sizeX-self.loc[0])
+        
+        # Lx = east/west world location
+        if len(self.sensoryConnections[15]) != 0:
+            self.sensoryValues[15] = self.loc[0]
+
+        # BD = nearest border distance
+        if len(self.sensoryConnections[16]) != 0:
+            xDist = min(self.loc[0], grid.sizeX-self.loc[0])
+            yDist = min(self.loc[1], grid.sizeY-self.loc[1])
+            self.sensoryValues[16] = min(xDist,yDist)
+            
+        # Ly = north/south world location
+        if len(self.sensoryConnections[17]) != 0:
+            self.sensoryValues[17] = self.loc[1]
+        
 
     # Run in this order!------------------------------------------------------------------------------------------------
     def sumInternalFromSensory(self):
@@ -170,12 +279,30 @@ class indiv:
                     self.actionOutputs[connection[1]] += self.internalOutputs[i] * self.internalWeights[i]
     # -------------------------------------------------------------------------------------------------------------------
 
+    def InternalTanH(self):
+        for i in range(len(self.internalOutputs)):
+            self.internalOutputs[i] = math.tanh(self.internalOutputs[i])
+
+    def ActionTanH(self):
+        for i in range(len(self.actionOutputs)):
+            self.actionOutputs[i] = math.tanh(self.actionOutputs[i])     
 
     def isAlive(self):
         return self.alive
     
     # use nnet to generate actions, and perform them
     def feedForward(self, simStep, grid):
-        pass
+        self.genSensoryData(grid,simStep)
+        self.sumInternalFromSensory()
+        self.sumActionFromSensory()
+
+        self.InternalTanH()
+
+        self.sumInternalFromInternal()       
+        self.sumActionFromInternal()
+
+        self.ActionTanH()
+
+        self.runActions()
 
     
