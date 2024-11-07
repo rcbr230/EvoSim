f86509d1a6464a6629f0c39ed781b8915a4c551e
rcbr230 <145787178+rcbr230@users.noreply.github.com>
2024-11-07 13:16:22 -0500

    Update Simulator.py
    WIP: culling and repopulating

--- src/Simulator.py
+++ src/Simulator.py
@@ -2,6 +2,7 @@
 This is the top level of the simulation that a user can interact with.
 
 """
+from enum import Enum, auto
 from Grid import Grid
 from Peeps import Peeps
 import tkinter as TK
@@ -15,6 +16,33 @@ GRID_Y = 128
 grid = Grid(GRID_X,GRID_Y)
 peeps = Peeps(POP_SIZE)
 generation = 0
+
+class SurvivalConditions(Enum):
+    LeftandRight = auto()
+    Center = auto()
+    Top = auto()
+    Bottom = auto()
+# refill pop to max size
+def repopulate(survPop):
+    for i in range(len(survPop),POP_SIZE):
+        pass
+
+# return an array of peep indexes that pass some condition
+def cull(grid, condition):
+    if condition == SurvivalConditions.LeftandRight:
+        leftBounds = grid.sizeX/4
+        rightBounds = grid.sizeX/4*3,grid.sizeX
+        survivingPop = [0]
+        highestIndex = 1
+        for i in range(len(grid.sizeY)):
+            for j in range(0,leftBounds):
+                if grid.gridInfo[i][j] != 0:
+                    survivingPop.append(grid.gridInfo[i][j])
+        for i in range(len(grid.sizeY)):
+            for j in range(rightBounds,grid.sizeX):
+                if grid.gridInfo[i][j] != 0:
+                    survivingPop.append(grid.gridInfo[i][j])
+    return survivingPop           
 
 def simStepOneIndividual(indiv, simStep, instance):
     indiv.age += 1
@@ -46,9 +74,9 @@ while generation < MAX_GENERATIONS:
                 simStepOneIndividual(peeps.getIndividual(index),i, index)
         print(str(i) + " ",end=" ")
     print("Generation " + generation)
-        
-    # CREATE NEW GEN FROM PREV GEN
 
+    # CREATE NEW GEN FROM PREV GEN
+    newGeneration = cull(grid, SurvivalConditions.LeftandRight)
+    newGeneration = repopulate(newGeneration)
     generation += 1
 root.mainloop()
-
